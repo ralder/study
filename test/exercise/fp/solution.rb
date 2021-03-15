9f7b3099ff514777759bacb709ab40f5f63e9c57
sergey.mishin <s.mishin@skyeng.ru>
2021-03-16 02:05:56 +0300

    2 - solve fp

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,20 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(films)
+        has_many_countries = ->(country) { country.split(',').length > 1 }
+
+        ratings = films.filter { |film| has_many_countries.call(film['country'] || '') }
+                       .map { |film| film['rating_kinopoisk'].to_f }
+                       .filter(&:positive?)
+
+        ratings.reduce(:+) / ratings.length
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        films.filter { |film| film['rating_kinopoisk'].to_f >= threshold }
+             .map { |film| film['name'].count('и') }
+             .reduce(:+)
       end
     end
   end
